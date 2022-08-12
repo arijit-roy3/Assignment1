a9051f47ad1b5f2a0f8a56accf7a63421d53bf79
arijit-roy3 <arijit.roy@perfios.com>
2022-08-12 15:26:13 +0530

    Second

---
+++ src/AccessModifier2/AccessModifiers2.java
@@ -0,0 +1,19 @@
+package AccessModifier2;
+import java.util.Scanner;
+import AccessModifier.AccessModifiers;
+public class AccessModifiers2 extends AccessModifiers{
+	public static void main(String[] args) {
+		Test obj1 = new Test();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please Enter the name");
+		String name=scanner.nextLine();
+		System.out.println("Please Enter the age");
+		int age=obj1.y=scanner.nextInt();
+		System.out.println("Please Enter the marks");
+		int marks =obj1.x=scanner.nextInt();
+		//obj1.checkAge(age);		
+		obj1.displayName(name);
+		//obj1.checkMarks(marks);		
+		
+		}
+}
---
+++ src/AccessModifier2/Test.java
@@ -0,0 +1,18 @@
+package AccessModifier2;
+
+import AccessModifier.AccessModifiers;
+
+public class Test extends AccessModifiers{
+	public void displayName(String name) {
+		System.out.println("Name: "+name);
+		
+	}
+	public void checkAge(int  age) {
+		System.out.println("Age: "+age);
+		
+	}
+	public void checkMarks(int marks) {
+		System.out.println("Marks: "+marks);
+		
+	}
+}
